bb24058ff80ece9bd4398e61937c47c122caba1d
Rupal Totale <rupaltotale@gmail.com>
2021-05-16 16:20:25 -0700

    Nuke markdown file, fix constant path, generate original file

--- code/generate_md.py
+++ code/generate_md.py
@@ -19,8 +19,16 @@ directory = os.fsencode("notebooks")
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".ipynb"):
+        # Determine markdown_prefix
         markdown_prefix = filename.replace(".ipynb", "").replace(" ", "-").lower()
         print(f"Converting {filename} to markdown files with prefix {markdown_prefix}")
+
+        # Cleaned file
         nb_body, resources = MarkdownExporter(config=c).from_filename(f"notebooks/{filename}")
         with open(f'markdown/{markdown_prefix}_cleaned.md', 'w') as writer:
+            writer.write(nb_body)
+
+        # original file
+        nb_body, resources = MarkdownExporter().from_filename(f"notebooks/{filename}")
+        with open(f'markdown/{markdown_prefix}_original.md', 'w') as writer:
             writer.write(nb_body)
--- csc466-education-app/src/constants.ts
+++ csc466-education-app/src/constants.ts
@@ -2,4 +2,4 @@ import process from "process";
 
 export const IS_DEV: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
-export const ASSETS_URL = "https://raw.githubusercontent.com/CSC466-Team7/csc466_project/main/markdown/";
+export const ASSETS_URL = "https://raw.githubusercontent.com/CSC466-Team7/csc466_project/main/code/markdown/";
